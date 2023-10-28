f0a5d1cecad5b221a2fe087f5b04f1d3d00e8515
Dylan Yang <osuyang.6294@gmail.com>
2023-10-28 16:55:20 -0400

    Added dots to map

--- map.js
+++ map.js
@@ -1,4 +1,4 @@
-const mapButton = document.querySelector('#dot');
+const mapButton = document.querySelector('#bookstore');
 
 mapButton.addEventListener('click', function() {
     window.location.href='location_pages/union_bank.html';
