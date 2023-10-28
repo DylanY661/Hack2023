ff3b1cca212556b28e35835b9f3b419c7310f892
Dylan Yang <osuyang.6294@gmail.com>
2023-10-28 19:52:34 -0400

    Added Links to Buttons

--- map.js
+++ map.js
@@ -1,11 +1,23 @@
 const mapButton = document.querySelector('#bookstore');
 const mapButton2 = document.querySelector('#north_entrance');
-const mapButton3 = document.querySelector('#US_Bank');
+const mapButton3 = document.querySelector('#US_bank');
 const mapButton4 = document.querySelector('#info_center');
-
+const mapButton5 = document.querySelector('#east_entrance');
+const mapButton6 = document.querySelector('#family_room');
+const mapButton7 = document.querySelector('#meeting_room');
+const mapButton8 = document.querySelector('#alumni');
+const mapButton9 = document.querySelector('#potter_plaza');
+const mapButton10 = document.querySelector('#great_hall');
+const mapButton11 = document.querySelector('#performance_hall');
+const mapButton12 = document.querySelector('#market');
+const mapButton13 = document.querySelector('#woodys');
+const mapButton14 = document.querySelector('#lounge');
+const mapButton15 = document.querySelector('#theatre');
+const mapButton16 = document.querySelector('#express');
+const mapButton17 = document.querySelector('#sloopys');
 
 mapButton.addEventListener('click', function() {
-    window.location.href='location_pages/osu_bookstore.html';
+    window.location.href='location_pages/bookstore.html';
 });
 
 mapButton2.addEventListener('click', function() {
@@ -13,11 +25,61 @@ mapButton2.addEventListener('click', function() {
 });
 
 mapButton3.addEventListener('click', function() {
-    window.location.href='location_pages/union_bank.html';
+    window.location.href='location_pages/US_bank.html';
 });
 
 mapButton4.addEventListener('click', function() {
     window.location.href='location_pages/info_center.html';
 });
 
+mapButton5.addEventListener('click', function() {
+    window.location.href='location_pages/east_entrance.html';
+});
 
+mapButton6.addEventListener('click', function() {
+    window.location.href='location_pages/family_room.html';
+});
+
+mapButton7.addEventListener('click', function() {
+    window.location.href='location_pages/meeting_room.html';
+});
+
+mapButton8.addEventListener('click', function() {
+    window.location.href='location_pages/alumni.html';
+});
+
+mapButton9.addEventListener('click', function() {
+    window.location.href='location_pages/potter_plaza.html';
+});
+
+mapButton10.addEventListener('click', function() {
+    window.location.href='location_pages/great_hall.html';
+});
+
+mapButton11.addEventListener('click', function() {
+    window.location.href='location_pages/performance_hall.html';
+});
+
+mapButton12.addEventListener('click', function() {
+    window.location.href='location_pages/market.html';
+});
+
+mapButton13.addEventListener('click', function() {
+    window.location.href='location_pages/woodys.html';
+});
+
+mapButton14.addEventListener('click', function() {
+    window.location.href='location_pages/lounge.html';
+});
+
+mapButton15.addEventListener('click', function() {
+    window.location.href='location_pages/theatre.html';
+});
+
+mapButton16.addEventListener('click', function() {
+    window.location.href='location_pages/express.html';
+});
+
+mapButton17.addEventListener('click', function() {
+    window.location.href='location_pages/sloopys.html';
+});
