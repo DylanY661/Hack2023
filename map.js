e1ed579071b47880daecc52a3c1a08962c1d3d0a
John Doe <iaustin0b@gmail.com>
2023-10-28 18:09:33 -0400

    upd

--- map.js
+++ map.js
@@ -3,3 +3,9 @@ const mapButton = document.querySelector('#bookstore');
 mapButton.addEventListener('click', function() {
     window.location.href='location_pages/union_bank.html';
 });
+
+const mapButton2 = document.querySelector('#north_entrance');
+
+mapButton.addEventListener('click', function() {
+    window.location.href='location_pages/east_entrance.html';
+});
