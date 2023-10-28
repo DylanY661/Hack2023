57cb5ce63ec3100cbceb0ac1f845b91aebbe4e64
John Doe <iaustin0b@gmail.com>
2023-10-28 18:57:23 -0400

    upd

--- map.js
+++ map.js
@@ -1,11 +1,23 @@
 const mapButton = document.querySelector('#bookstore');
+const mapButton2 = document.querySelector('#north_entrance');
+const mapButton3 = document.querySelector('#US_Bank');
+const mapButton4 = document.querySelector('#info_center');
+
 
 mapButton.addEventListener('click', function() {
+    window.location.href='location_pages/osu_bookstore.html';
+});
+
+mapButton2.addEventListener('click', function() {
+    window.location.href='location_pages/north_entrance.html';
+});
+
+mapButton3.addEventListener('click', function() {
     window.location.href='location_pages/union_bank.html';
 });
 
-const mapButton2 = document.querySelector('#north_entrance');
-
-mapButton.addEventListener('click', function() {
-    window.location.href='location_pages/east_entrance.html';
+mapButton4.addEventListener('click', function() {
+    window.location.href='location_pages/info_center.html';
 });
+
+
